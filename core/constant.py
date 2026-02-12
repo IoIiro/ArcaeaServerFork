8f84477aa44ce7fd49c3472680e9c15e3f4b1c00
creveld <rlarhsid11@gmail.com>
2026-02-12 15:52:24 +0900

    okay, why weren't we migrating this?
    - Added a missing `user_mission` table in the migration list.

--- core/constant.py
+++ core/constant.py
@@ -123,7 +123,7 @@ class Constant:
     ]
 
     DATABASE_MIGRATE_TABLES = ['user', 'friend', 'best_score', 'recent30', 'user_world', 'item', 'user_item', 'purchase', 'purchase_item', 'user_save', 'user_world_map',
-                               'login', 'present', 'user_present', 'present_item', 'redeem', 'user_redeem', 'redeem_item', 'api_login', 'chart', 'user_course', 'user_char', 'user_role']
+                               'login', 'present', 'user_present', 'present_item', 'redeem', 'user_redeem', 'user_mission', 'redeem_item', 'api_login', 'chart', 'user_course', 'user_char', 'user_role']
 
     LOG_DATABASE_MIGRATE_TABLES = ['cache', 'user_score', 'user_rating']
 
