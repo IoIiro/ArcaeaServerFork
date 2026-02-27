5e7dfeba9ddbd6c63ea0f2d95e11be1b5055a30b
ariidesu <shiinazzz.burga@gmail.com>
2026-02-28 00:08:53 +0700

    fix db

--- database/init/tables.sql
+++ database/init/tables.sql
@@ -38,7 +38,7 @@ beyond_boost_gauge real default 0,
 kanae_stored_prog real default 0,
 mp_notification_enabled int default 1,
 insight_state int default 0,
-custom_banner text
+custom_banner text,
 is_allow_marketing_email int default 0
 );
 create table if not exists login(access_token text,
